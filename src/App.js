dfa4bfc24b281cd32fd551db4df9b4ca9a97317e
sajibcse68 <sajibcse68@gmail.com>
2021-02-28 18:34:51 +0600

    Take input message and show messages, save msg in memory

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import { Form, Button } from 'react-bootstrap';
+
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [username, setUsername] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setMessages([...messages, input]);
+    setInput('');
+  };
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">Sajib: Hey Welcome 🚀!</div>
+      <div className="msg-cont">
+        <Form className="msg-form" onSubmit={handleSubmit}>
+          <Form.Group className="email-input">
+            {/* <Form.Label>Email address</Form.Label> */}
+            <Form.Control
+              ref={(input) => input && input.focus()}
+              type="text"
+              value={input}
+              onChange={handleInputChange}
+              placeholder="Enter a message"
+            />
+          </Form.Group>
+
+          {/* <Button variant="primary" type="submit">
+            Submit
+          </Button> */}
+        </Form>
+
+        <div className="show-msg">
+          {messages.map((msg) => (
+            <p>{msg}</p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
